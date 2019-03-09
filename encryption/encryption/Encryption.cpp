2032c2d7295647d924aae2bd8897b010e7e6138c
D.Hill <hillmarsoc@yahoo.com>
2019-03-09 02:51:44 -0500

    Update Encryption.cpp

--- encryption/encryption/Encryption.cpp
+++ encryption/encryption/Encryption.cpp
@@ -1,7 +1,7 @@
 #include "Encryption.h"
 Encryption::Encryption() {
 	this->userMessage = "test.";
-	this->userPin = "password123";
+	this->userPin = "Password123";
 }
 Encryption::Encryption(string pin, string message) {
 	this->userMessage = message;
@@ -9,14 +9,9 @@ Encryption::Encryption(string pin, string message) {
 }
 //GETTERS:
 string  Encryption::getPin(void) {
-
 	return this->userPin;
 }
 string Encryption::getMessage(void) {
-	/*string sentence;
-	cout<<"END YOUR TEXT WITH A PERIOD.\n\t";
-	cout<<"ENTER YOUR TEXT: ";
-	getline(std::cin, sentence);*/
 	return this->userMessage;
 }
 //SETTERS:
@@ -24,20 +19,20 @@ void Encryption::setPin(string pin) {
 	this->userPin = pin;
 }
 void Encryption::setMessage(string userMessage) {
+	
 	this->userMessage = userMessage;//ENSURE THERE IS A PERIOD AT THE END OF THE STRING
 }
 //METHODS:
 string Encryption::cipherMessage() {
 	unsigned char key = 11001100;
-	int i = 0, counter = 0;
-	while (this->userMessage[i] != 46)//READ STRING UNTIL PERIOD IS FOUND
+	int counter = 0;
+	while (!this->userMessage.end())//READ STRING UNTIL THE END
 	{
-		counter++;//LIMIT FOR THE FOR LOOP
-		i++;//INCREMENTS THROUGH THE STRING
+		counter++;//NUMBER OF CHAR IN STRING	
 	}
 	for (int currentLetter = 0; currentLetter < counter; currentLetter++)
 	{
-		userMessage[currentLetter] ^= key;
+		userMessage[currentLetter] ^= key;//BITWISE XOR
 	}
 
 	return userMessage;//return encrypted string 
